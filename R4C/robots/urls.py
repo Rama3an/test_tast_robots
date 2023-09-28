11ed4874570740250110c3bd2bb84cee13c6b945
Ramazan Abakarov <abakarov-roma@mail.ru>
2023-09-28 23:26:25 +0500

    Добавление проекта на github

---
+++ R4C/robots/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from robots.views import *
+
+urlpatterns = [
+    path('api/updaterobots', csrf_exempt(PutNewRobots.as_view()))
+]
